e4382cf52db8783e721ce24b6f1705e906924b5b
ziw <zijnuw@gmail.com>
2019-06-16 18:02:04 -0500

    model_search network modified

--- cnn/model_search.py
+++ cnn/model_search.py
@@ -71,7 +71,7 @@ class Network(nn.Module):
 
     C_curr = stem_multiplier*C
     self.stem = nn.Sequential(
-      nn.Conv2d(3, C_curr, 3, padding=1, bias=False),
+      nn.Conv2d(1, C_curr, 1, padding=1, bias=False),
       nn.BatchNorm2d(C_curr)
     )
  
