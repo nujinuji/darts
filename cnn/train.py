b22159e99e4c2b07755a48a167dc9c937d11678b
tsuyoikaze <tuyoikaze@gmail.com>
2019-08-30 13:23:52 -0400

    testing new dataset

--- cnn/train.py
+++ cnn/train.py
@@ -17,39 +17,40 @@ import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
 from model import NetworkCIFAR as Network
 
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser("cifar")
+  parser.add_argument('--annofile', type=str, default='../dlprb_train/RNCMPT00001.txt.annotations.RNAcontext-sample', help='location of the annotation file')
+  parser.add_argument('--seqfile', type=str, default='../dlprb_train/RNCMPT00001.txt.sequences.RNAcontext.clamp-sample', help='location of the sequence file')
+  parser.add_argument('--batch_size', type=int, default=96, help='batch size')
+  parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
+  parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+  parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
+  parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
+  parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+  parser.add_argument('--epochs', type=int, default=600, help='num of training epochs')
+  parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
+  parser.add_argument('--layers', type=int, default=20, help='total number of layers')
+  parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
+  parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
+  parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
+  parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+  parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+  parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
+  parser.add_argument('--save', type=str, default='EXP', help='experiment name')
+  parser.add_argument('--seed', type=int, default=0, help='random seed')
+  parser.add_argument('--arch', type=str, default='DARTS_NEW', help='which architecture to use')
+  parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+  args = parser.parse_args()
 
-parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=96, help='batch size')
-parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
-parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
-parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=600, help='num of training epochs')
-parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
-parser.add_argument('--layers', type=int, default=20, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
-parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
-parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
-parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
-parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
-parser.add_argument('--save', type=str, default='EXP', help='experiment name')
-parser.add_argument('--seed', type=int, default=0, help='random seed')
-parser.add_argument('--arch', type=str, default='DARTS_NEW', help='which architecture to use')
-parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
-args = parser.parse_args()
+  args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
+  utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
 
-args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
-utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
-
-log_format = '%(asctime)s %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-    format=log_format, datefmt='%m/%d %I:%M:%S %p')
-fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
-fh.setFormatter(logging.Formatter(log_format))
-logging.getLogger().addHandler(fh)
+  log_format = '%(asctime)s %(message)s'
+  logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+      format=log_format, datefmt='%m/%d %I:%M:%S %p')
+  fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+  fh.setFormatter(logging.Formatter(log_format))
+  logging.getLogger().addHandler(fh)
 
 CIFAR_CLASSES = 2
 
@@ -57,99 +58,71 @@ CIFAR_CLASSES = 2
 CUTOFF = 0
 MAX_LEN = 41
 
-def zero_padding(holder, length, separator):
-  """Make shorter sequences zero-padded
+class BindingDataset(torch.utils.data.Dataset):
+  def __loader(annofile, seqfile):
+    """Load data from given path
 
-  Parameters
-  ----------
-  holder : str
-    zeros for matrix padding
-  length : int
-    length that need to pad at the end of shorter sequences
-  separator : str
-    tab separated
+    Parameters
+    ----------
+    annofile : str
+      file name of annotation file. format: <seq> <struct matrix>
 
-  Returns
-  -------
-  str
-    sequence with zero padding
-  """
-  return separator.join([holder] * int(length))
+    seqfile : str
+      file name of sequence file. format: <bind score> <seq>
 
+    Returns
+    -------
+    tuple of numpy array and str
+      2d data matrix and its label
+    """
+    '''
+    lbl = ['bind', 'not_bind'].index(path.split('/')[-2]) 
+    csv = pd.read_csv(path, sep='\t', header = None).values
+    
+    data = np.zeros((2, 5, 41))
+    data[0, :4, :] = csv[:4, :]
+    data[1, :, :] = csv[4:, :]
+    
+    data = csv
+    '''
+    res = []
+    affinity = []
+    with open(annofile) as anno_file:
+      with open(seqfile) as seq_file:
+        for seq_line in seq_file:
+          
+          # Get affinity and sequence; then transform sequence to numpy array of characters  
+          affinity.append(float(seq_line.split(' ')[0]))
+          seq = anno_file.readline()[1:].strip()
+          seq_numpy = np.array(list(seq))
 
-def loader(annofile, seqfile):
-  """Load data from given path
+          # Create empty 2D matrix and fill with one-hot representations
+          mat = np.zeros((9, MAX_LEN))
+          for i, c in enumerate('ATCG'):
+            mat[i, :len(seq)] = (seq_numpy == c).astype(float)
 
-  Parameters
-  ----------
-  annofile : str
-    file name of annotation file. format: <seq> <struct matrix>
+          # Append profile
+          for i in range(4, 9):
+            profile = anno_file.readline().lstrip().strip().split('\t')
+            mat[i, :len(seq)] = [float(x) for x in profile]
 
-  seqfile : str
-    file name of sequence file. format: <bind score> <seq>
+          res.append([mat])
+    return torch.tensor(res, dtype=torch.float), torch.tensor(affinity, dtype=torch.float)
 
-  Returns
-  -------
-  tuple of numpy array and str
-    2d data matrix and its label
-  """
-  '''
-  lbl = ['bind', 'not_bind'].index(path.split('/')[-2]) 
-  csv = pd.read_csv(path, sep='\t', header = None).values
-  
-  data = np.zeros((2, 5, 41))
-  data[0, :4, :] = csv[:4, :]
-  data[1, :, :] = csv[4:, :]
-  
-  data = csv
-  '''
-  res = []
-  affinity = []
-  with open(annofile) as anno_file:
-    with open(seqfile) as seq_file:
-      for seq_line in seq_file:
-        
-        # Get affinity and sequence; then transform sequence to numpy array of characters  
-        affinity.append(float(seq_line.split(' ')[0]))
-        seq = anno_file.readline()[1:].strip()
-        seq_numpy = np.array(list(seq))
+  def __init__(self, annofile, seqfile, transform = None):
+    self.dataset = __loader(annofile, seqfile)
 
-        # Create empty 2D matrix and fill with one-hot representations
-        mat = np.zeros((9, MAX_LEN))
-        for i, c in enumerate('ATCG'):
-          mat[i, :len(seq)] = (seq_numpy == c).astype(float)
+  def __len__(self):
+    return self.dataset.shape[0]
 
-        # Append profile
-        for i in range(4, 9):
-          profile = anno_file.readline().lstrip().strip().split('\t')
-          mat[i, :len(seq)] = [float(x) for x in profile]
+  def __getitem__(self, idx):
+    return self.dataset[idx, :, :, :]
 
-        res.append([mat])
-  return torch.tensor(res, dtype=torch.float), torch.tensor(affinity, dtype=torch.float)
+def create_queue(data, batch_size):
 
 
-def transform(d):
-  """Transform data to tensor from NxHxW to NxCxHxW by adding 1 dimension
 
-  Parameters
-  ----------
-  d : pytorch.tensor
-    3d matrix of input sequence data
-
-  Returns
-  -------
-  tuple of torch.tensor and str
-    4d matrix of float with its label
-  """
-  data, label = d[0], d[1]
-  try:
-    return torch.tensor(data, dtype=torch.float32).view(1, 9, 41), label
-  except ValueError:
-    sys.stderr.write(str(data))
-    return 0
-
-
-def main():
+def main(args):
   if not torch.cuda.is_available():
     logging.info('no gpu device available')
     sys.exit(1)
@@ -178,14 +151,15 @@ def main():
       weight_decay=args.weight_decay
       )
 
-  train_transform, valid_transform = utils._data_transforms_cifar10(args)
-  train_data = dset.DatasetFolder(args.data, loader, ['ext'], transform=transform)
+  train_data = BindingDataset(args.annofile, args.seqfile)
 
   num_train = len(train_data)
   indices = list(range(num_train))
   split = int(np.floor(0.7 * num_train))
 
+  print(num_train)
 
+  '''
   train_queue = torch.utils.data.DataLoader(
       train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
 
@@ -206,6 +180,7 @@ def main():
     logging.info('valid_acc %f', valid_acc)
 
     utils.save(model, os.path.join(args.save, 'weights.pt'))
+  '''
 
 
 def train(train_queue, model, criterion, optimizer):
@@ -269,5 +244,5 @@ def infer(valid_queue, model, criterion):
 
 
 if __name__ == '__main__':
-  main() 
+  main(args) 
 
